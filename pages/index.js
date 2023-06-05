97d52fc0ccf1ecd4570cec7f066caec483d3cf97
Mohd Ejaz Siddiqui <mohdejazsiddiqui@.com>
2023-06-05 12:59:59 +0400

    added footer and fixed the responsiveness

--- pages/index.js
+++ pages/index.js
@@ -2,16 +2,17 @@ import Head from 'next/head'
 import ListIngredients from "@/components/ListIngredients";
 import SearchBox from "@/components/SearchBox";
 import {useState} from "react";
+import Link from "next/link";
 
 export default function Home() {
 	const [highlightIng, setHighlightIng] = useState([])
 	return (
-		<main className={`min-h-screen`}>
+		<main className={`min-h-screen flex flex-col`}>
 			<Head>
 				<title>Semantic Search</title>
 			</Head>
-			<div className={'lg:p-5 pt-0 mx-auto border'}>
-				<div className={'text-purple-500 text-3xl px-3 mb-4'}>
+			<div className={'pt-0 w-full lg:max-w-7xl mx-auto flex-grow'}>
+				<div className={'text-purple-500 text-3xl px-3 my-4'}>
 					<h1>Semantic Search</h1>
 				</div>
 				<div className={'w-full grid grid-cols-1 lg:grid-cols-3'}>
@@ -22,6 +23,9 @@ export default function Home() {
 						<ListIngredients ingsArr={highlightIng}/>
 					</div>
 				</div>
+			</div>
+			<div className={'flex-shrink-0 text-center text-md p-4 border-t'}>
+				Created by <Link className={'text-purple-500'} href={"https://www.mejaz.in"} target={'_blank'} rel={'noopener noreferrer'}>Mohd Ejaz Siddiqui</Link>
 			</div>
 		</main>
 	)
